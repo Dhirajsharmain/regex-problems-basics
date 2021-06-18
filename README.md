ea4bffc869310af932b08fe84cf84debe563a2d3
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-18 20:10:47 +0530

    Refactored: naming conventions and commeting

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 
 _Description :_ As a User need to enter a valid First Name
 - First name starts with Cap and has minimum 3 characters
+  
+
+  **UC2-ValidateLastName**
+
+_Description :_ As a User need to enter a valid Last Name
+- Last name starts with Cap and has minimum 3 characters
--- bridgelabz/UserRegistration.java
+++ bridgelabz/UserRegistration.java
@@ -1,17 +1,38 @@
+/**
+ * ****************************************************************************
+ * Purpose: This Class User Registration is implemented to ensure all validations
+ * are in place during the User Entry
+ *
+ * @author Dhiraj
+ * @version 1.0
+ * @since 18-06-2021 **********************************************************
+ */
 package bridgelabz;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+
+    /**
+     * Main method is starting point os this program.
+     * @param args
+     */
     public static void main(String[] args) {
         String name = "Abc";
         System.out.println(ValidateFirstName(name));
     }
 
+    /**
+     * Method for validating the user first name using regex.
+     * @param firstName
+     * @return
+     */
     private static boolean ValidateFirstName(String firstName){
         Pattern p = Pattern.compile("^[A-Z]+[a-zA-Z]{2,}$");
         Matcher m = p.matcher(firstName);
         return m.matches();
     }
+
+
 }
