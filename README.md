dbaaa92338f2a73aa3785de4a258f39d10513df6
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-18 19:54:15 +0530

    Added: validate First Name

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # regex-problems-basics
+
+**UC1-ValidateFirstName**
+
+_Description :_ As a User need to enter a valid First Name
+- First name starts with Cap and has minimum 3 characters
--- bridgelabz/UserRegistration.java
+++ bridgelabz/UserRegistration.java
@@ -1,4 +1,17 @@
 package bridgelabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    public static void main(String[] args) {
+        String name = "Abc";
+        System.out.println(ValidateFirstName(name));
+    }
+
+    private static boolean ValidateFirstName(String firstName){
+        Pattern p = Pattern.compile("^[A-Z]+[a-zA-Z]{2}$");
+        Matcher m = p.matcher(firstName);
+        return m.matches();
+    }
 }
