b805893dc0c0f295b79c060d4092bc2207f79964
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-20 10:59:58 +0530

    Refactored: sample emails validation using regex

--- bridgelabz/ValidateEmails.java
+++ bridgelabz/ValidateEmails.java
@@ -19,13 +19,17 @@ public class ValidateEmails {
      * @param email : sample emails
      * @return
      */
-    private static boolean validateEmails(String regex, String email){
+    private static boolean validateEmails(String email){
+        String regex = "^[a-z]{2,}[0-9a-z]+(?=.*[_.-])(?=.*[a-z0-9])[@]+[a-z0-1]{1,}[.]{1}[a-z]{2,4}([.]{0,1}[a-z]{2}){0,1}$";
         Pattern pattern = Pattern.compile(regex);
+        if (email == null) {
+            return false;
+        }
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
 
     public static void main(String[] args) {
-        System.out.println(validateEmails("^[a-zA-Z]+@+[a-zA-Z]+[.][a-zA-Z]*$","abc@yahoo.com"));
+        System.out.println(validateEmails("abc@yahoo.com"));
     }
 }
